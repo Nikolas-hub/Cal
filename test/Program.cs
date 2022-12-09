3387aee2383016c86608ec025256a7815ffb6dca
Nikolas-hub <nikola765040@gmail.com>
2022-12-10 04:45:31 +0500

    Создал калькулятор

---
+++ test/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace test
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Выберите операцию:\n1. Сложение\n2. Вычитание\n3. Деление\n4. Выход");
+                string number = Console.ReadLine();
+                Console.Clear();
+
+                if (number == "1")
+                {
+                    try
+                    {
+                        Console.WriteLine("Введите число а:");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.Clear();
+
+                        Console.WriteLine("Введите число b:");
+                        double b = double.Parse(Console.ReadLine());
+                        Console.Clear();
+
+                        Console.WriteLine($"Ответ: {a + b}");
+                        Console.ReadKey();
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Вводите только число!\nНажмите любую клавишу для продолжения");
+                        Console.ReadKey();
+                    }
+                }
+                else if (number == "2")
+                {
+                    try
+                    {
+                        Console.WriteLine("Введите число а:");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.Clear();
+
+                        Console.WriteLine("Введите число b:");
+                        double b = double.Parse(Console.ReadLine());
+                        Console.Clear();
+
+                        Console.WriteLine($"Ответ: {a - b}");
+                        Console.ReadLine();
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Вводите только число!\nНажмите любую клавишу для продолжения");
+                        Console.ReadKey();
+                    }
+                }
+                else if (number == "3")
+                {
+                    try
+                    {
+                        Console.WriteLine("Введите число а:");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.Clear();
+
+                        Console.WriteLine("Введите число b:");
+                        double b = double.Parse(Console.ReadLine());
+                        Console.Clear();
+
+                        Console.WriteLine($"Ответ: {a / b}");
+                        Console.ReadLine();
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Вводите только число!\nНажмите любую клавишу для продолжения");
+                        Console.ReadKey();
+                    }
+                }
+                else if (number == "4") break;
+                else
+                {
+                    Console.WriteLine("Выберите существующую операцию!\nНажмите любую клавишу для продолжения");
+                    Console.ReadKey();
+                }
+            }
+
+        }
+    }
+
+}
